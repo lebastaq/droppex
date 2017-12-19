88102a6fa29215ab6752adad009cc76c1db9d903
lebastaq <qlebastard@gmail.com>
2017-12-19 17:16:44 +0000

    deleted useless function that made the storage pool download a file - the storage pool now starts a pool of downloaders that wait for inputs

--- StorageController/src/main/java/com/fcup/StorageController.java
+++ StorageController/src/main/java/com/fcup/StorageController.java
@@ -27,33 +27,6 @@ public class StorageController extends StoragePoolsManager {
         grpcServer = new GrpcServer();
     }
 
-    // TODO implement grpc app server <-> storage controller (e.g. implement grpc client......)
-    // TODO add the good chunk ID (retrieved from app server)
-    // called via Grpc by the app server
-    // note: the app server or storage pool MUST have started a seeder before calling...
-    public void makeStoragePoolDownloadAFileFromTheAppServer() throws Exception {
-        String chunkID = "Test chunk ID...."; // TODO retrieve from grpc call from app server
-        String poolAddress = "127.0.0.1"; // TODO retrieve from grpc call from app server
-        int poolPort = 50051; // same... this is the default one
-
-        // TODO retrieve this from the grpc call
-        String destinationAdress = "";
-        int destinationPort = 26001;
-
-        Info request = Info.newBuilder().setIp(destinationAdress).setPort(destinationPort).setChunkId(chunkID).build(); // todo host = storage pool - how to get it ?
-        Status response;
-        try {
-            DownloadStarter downloadStarter = new DownloadStarter(poolAddress, poolPort);
-            response = downloadStarter.getAnswer(request);
-        } catch (StatusRuntimeException e) {
-            // TODO manage this: re-construct the chunks from FEC, and dispatch them in the other pools ... ?
-            System.err.println("Could not start downloader in storage pool for chunk " + chunkID);
-            throw e;
-        }
-        System.out.println("Response: " + response.getOk());
-
-    }
-
     public void start() throws Exception {
         grpcServer.startGrpcServer();
         eventLoop();
