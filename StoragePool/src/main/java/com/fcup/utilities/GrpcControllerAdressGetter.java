c73acd7abb47666f775e3435ce8825c29f714c7c
lebastaq <qlebastard@gmail.com>
2017-12-21 10:41:50 +0000

    Started adding grpc code for storage controller apdress update

--- StoragePool/src/main/java/com/fcup/utilities/GrpcControllerAdressGetter.java
+++ StoragePool/src/main/java/com/fcup/utilities/GrpcControllerAdressGetter.java
@@ -2,14 +2,34 @@ package com.fcup.utilities;
 
 import com.fcup.StoragePool;
 import com.fcup.generated.*;
+import com.fcup.generated.adressChangedStatus;
+import com.fcup.generated.storageControllerInfo;
 import io.grpc.stub.StreamObserver;
 
-public class GrpcControllerAdressGetter extends downloaderGrpc.downloaderImplBase{
+public class GrpcControllerAdressGetter extends addressgetterGrpc.adressgetterImplBase{
     private StoragePool storagePool;
 
     public GrpcControllerAdressGetter(StoragePool storagePool)
     {
         this.storagePool = storagePool;
+    }
+
+    @Override
+    public void getAdress(storageControllerInfo request, StreamObserver<adressChangedStatus> responseObserver) {
+        responseObserver.onNext(changeAdress(request));
+        responseObserver.onCompleted();
+    }
+
+    private adressChangedStatus changeAdress(storageControllerInfo request) {
+        DownloadInfo downloadInfo = new DownloadInfo(request.getIp(), request.getPort(), request.getChunkId());
+
+//        System.out.println("Just got a grpc request: " + request.getIp() + ", " + request.getPort() + ", " + request.getChunkId());
+//
+//        Downloader downloader = new Downloader(STORAGE_FOLDER, downloadInfo);
+//        downloader.run();
+//        System.out.println("Started downloader !");
+//
+//        return Status.newBuilder().setOk(true).build();
     }
 
     @Override
@@ -24,13 +44,13 @@ public class GrpcControllerAdressGetter extends downloaderGrpc.downloaderImplBas
     private Status startDownload(Info request) {
         DownloadInfo downloadInfo = new DownloadInfo(request.getIp(), request.getPort(), request.getChunkId());
 
-        System.out.println("Just got a grpc request: " + request.getIp() + ", " + request.getPort() + ", " + request.getChunkId());
-
-        Downloader downloader = new Downloader(STORAGE_FOLDER, downloadInfo);
-        downloader.run();
-        System.out.println("Started downloader !");
-
-        return Status.newBuilder().setOk(true).build();
+//        System.out.println("Just got a grpc request: " + request.getIp() + ", " + request.getPort() + ", " + request.getChunkId());
+//
+//        Downloader downloader = new Downloader(STORAGE_FOLDER, downloadInfo);
+//        downloader.run();
+//        System.out.println("Started downloader !");
+//
+//        return Status.newBuilder().setOk(true).build();
     }
 
 }
